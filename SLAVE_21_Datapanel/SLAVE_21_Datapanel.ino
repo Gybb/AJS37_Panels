5025139c4bec6b529ffa742b8da838bc36d2f4db
Gybb <fredrik_ljung@live.se>
2021-03-14 20:00:32 +0100

    Add files via upload

---
+++ SLAVE_21_Datapanel/SLAVE_21_Datapanel.ino
@@ -0,0 +1,62 @@
+//#define DCSBIOS_IRQ_SERIAL
+#define DCSBIOS_RS485_SLAVE 21
+#define TXENABLE_PIN 2
+
+#include "DcsBios.h"
+
+DcsBios::Switch2Pos datapanelKey1("DATAPANEL_KEY_1", 12);
+
+DcsBios::Switch2Pos datapanelKey2("DATAPANEL_KEY_2", 11);
+
+DcsBios::Switch2Pos datapanelKey3("DATAPANEL_KEY_3", 10);
+
+DcsBios::Switch2Pos datapanelKey4("DATAPANEL_KEY_4", 9);
+
+DcsBios::Switch2Pos datapanelKey5("DATAPANEL_KEY_5", 8);
+
+DcsBios::Switch2Pos datapanelKey6("DATAPANEL_KEY_6", 7);
+
+DcsBios::Switch2Pos datapanelKey7("DATAPANEL_KEY_7", 6);
+
+// PIN 5 for PWM LED
+int PanelLight_pin = 5;
+int PanelLed_Value;
+
+DcsBios::Switch2Pos datapanelKey8("DATAPANEL_KEY_8", 4);
+
+DcsBios::Switch2Pos datapanelKey9("DATAPANEL_KEY_9", 3);
+
+DcsBios::Switch2Pos ck37RensaClear("CK37_RENSA_CLEAR", A0);
+
+DcsBios::Switch2Pos datapanelKey0("DATAPANEL_KEY_0", A1);
+
+DcsBios::Switch2Pos dataInOut("DATA_IN_OUT", A2);
+
+//DcsBios::Switch2Pos rensaButtonCover("RENSA_BUTTON_COVER", A3);
+
+DcsBios::AnalogMultiPos datapanelSelector("DATAPANEL_SELECTOR", A7, 6, (1023/6));
+
+
+void setup() {
+  
+  DcsBios::setup();
+ //  pinMode(PanelLight_pin, OUTPUT);
+}
+
+void onMainElectricPowerChange(unsigned int newValue) {
+    if (newValue==0){
+      PanelLed_Value = 0; }
+}
+DcsBios::IntegerBuffer mainElectricPowerBuffer(0x4650, 0x0008, 3, onMainElectricPowerChange);
+  
+void onPanelLightsChange(unsigned int newValue) {
+      PanelLed_Value = map(newValue, 0, 65535, 0, 200);
+     analogWrite(PanelLight_pin, PanelLed_Value);
+}
+DcsBios::IntegerBuffer panelLightsBuffer(0x464c, 0xffff, 0, onPanelLightsChange);
+
+
+
+void loop() {
+  DcsBios::loop();
+  }
---
+++ SLAVE_40_Destinationsindikator/SLAVE_40_Destinationsindikator.ino
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+#include <UTFT.h>
+#include <SPI.h>
+//#include <avr/pgmspace.h>
+//#define DCSBIOS_IRQ_SERIAL
+#define DCSBIOS_RS485_SLAVE 40
+#define TXENABLE_PIN 2
+#include "DcsBios.h"
+
+const char* Bxred = "5";
+const char* Ured = "7";
+
+UTFT myGLCD(ST7735S_4L_80160,11,13,10,8,9);  //ST7735 0.96" 80x160(RGB)IPS
+
+extern uint8_t DEST1[];
+extern uint8_t DEST2[];
+
+void setup()
+{
+  myGLCD.InitLCD(LANDSCAPE);
+  //myGLCD.lcdOff();
+  //myGLCD.setBrightness(1);
+  //myGLCD.setContrast(1);
+  myGLCD.clrScr();
+  myGLCD.setBackColor(0, 0, 0);
+  myGLCD.setColor(245, 195, 80);
+  myGLCD.setFont(DEST2);
+  myGLCD.print("37", 40, 18);
+  myGLCD.clrScr();
+
+  DcsBios::setup();
+}
+//Left_Indicator
+void onajs37DestIndicatorDataVChange(char* newValue) {
+  myGLCD.setFont(DEST1);
+    
+    if (strcmp(newValue, "7") == 0){
+      //om värdet är 7 så visa röd färg
+     myGLCD.setColor(150, 0, 0);
+     myGLCD.print(newValue, 12, 18);
+    }
+    else{
+      myGLCD.setColor(245, 195, 80);
+      myGLCD.print(newValue, 12, 18);
+    }
+}
+DcsBios::StringBuffer<1> ajs37DestIndicatorDataVBuffer(0x46a8, onajs37DestIndicatorDataVChange);
+
+//Right_Indicator
+void onajs37DestIndicatorDataHChange(char* newValue) {
+    myGLCD.setFont(DEST2);
+    myGLCD.setColor(245, 195, 80);
+    myGLCD.print(newValue, 95, 18);
+}
+DcsBios::StringBuffer<1> ajs37DestIndicatorDataHBuffer(0x46aa, onajs37DestIndicatorDataHChange);
+
+
+void loop()
+{
+   DcsBios::loop();
+
+}
