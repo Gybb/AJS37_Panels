f005388ecc953d8f795e166e5c92c2dbbdbd571c
Gybb <fredrik_ljung@live.se>
2021-03-26 21:41:17 +0100

    Check switch states, insert missed B6

--- SLAVE_23_Navigeringspanel/SLAVE_23_Navigeringspanel.ino
+++ SLAVE_23_Navigeringspanel/SLAVE_23_Navigeringspanel.ino
@@ -4,19 +4,21 @@
 
 #include "DcsBios.h"
 
-DcsBios::Switch2Pos dmeSelector("DME_SELECTOR", 12);
+DcsBios::Switch2Pos dmeSelector("DME_SELECTOR", 13);
 
-DcsBios::Switch2Pos radarAltimeterPower("RADAR_ALTIMETER_POWER", 11);
+DcsBios::Switch2Pos radarAltimeterPower("RADAR_ALTIMETER_POWER", 12);
 
-DcsBios::Switch2Pos navSelectBtnB1("NAV_SELECT_BTN_B1", 10);
+DcsBios::Switch2Pos navSelectBtnB1("NAV_SELECT_BTN_B1", 11);
 
-DcsBios::Switch2Pos navSelectBtnB2("NAV_SELECT_BTN_B2", 9);
+DcsBios::Switch2Pos navSelectBtnB2("NAV_SELECT_BTN_B2", 10);
 
-DcsBios::Switch2Pos navSelectBtnB3("NAV_SELECT_BTN_B3", 8);
+DcsBios::Switch2Pos navSelectBtnB3("NAV_SELECT_BTN_B3", 9);
 
-DcsBios::Switch2Pos navSelectBtnB4("NAV_SELECT_BTN_B4", 7);
+DcsBios::Switch2Pos navSelectBtnB4("NAV_SELECT_BTN_B4", 8);
 
-DcsBios::Switch2Pos navSelectBtnB5("NAV_SELECT_BTN_B5", 6);
+DcsBios::Switch2Pos navSelectBtnB5("NAV_SELECT_BTN_B5", 7);
+
+DcsBios::Switch2Pos navSelectBtnB6("NAV_SELECT_BTN_B6", 6);
 
 // PIN 5 for PWM LED
 int PanelLight_pin = 5;
@@ -35,7 +37,10 @@ DcsBios::Switch2Pos navSelectBtnLMal("NAV_SELECT_BTN_L_MAL", A2);
 DcsBios::Switch2Pos navSelectBtnLs("NAV_SELECT_BTN_LS", A3);
 
 DcsBios::AnalogMultiPos tilsChannelSelect("TILS_CHANNEL_SELECT", A7, 11, (1023/11));
-
+//*******************NAVPANEL SLUT******************************
+//----------Check switch states polls-----------------
+unsigned int g_iInitIntervalCounter = 0;
+unsigned int g_iForcePollCycles = 10;
 
 void setup() {
   
@@ -59,4 +64,21 @@ DcsBios::IntegerBuffer panelLightsBuffer(0x4678, 0xffff, 0, onPanelLightsChange)
 
 void loop() {
   DcsBios::loop();
+         if ( g_iForcePollCycles > 0 )
+    { 
+        //repeat poll for this many cycles
+        if ( ++g_iInitIntervalCounter == 0 ) 
+        {  PollAllControls();     //main loop 1->65535->0 then polls
+           g_iForcePollCycles--;
+        }
+
+    }
+
+}
+
+void PollAllControls()
+{
+    dmeSelector.pollInputCurrent();
+    radarAltimeterPower.pollInputCurrent();
+    tilsChannelSelect.pollInputCurrent();
   }
