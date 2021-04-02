90d8bfd84599b203c065f87ddd1d32a83a93818b
Gybb <fredrik_ljung@live.se>
2021-04-02 21:53:16 +0200

    Panel lights work a intended

--- SLAVE_4_Radarpanel/SLAVE_04_Radarpanel.ino
+++ SLAVE_4_Radarpanel/SLAVE_04_Radarpanel.ino
@@ -24,34 +24,60 @@ DcsBios::AnalogMultiPos masterModeSelect("MASTER_MODE_SELECT", A7, 6, (1023/6));
 DcsBios::AnalogMultiPos antiJamMode("ANTI_JAM_MODE", A6, 7, (1023/7));
 
 //*******************  SLUT RADARPANEL  ***********************/
-//--------------Check switch states polls-----------
+//---------- Check switch states --------------
 unsigned int g_iInitIntervalCounter = 0;
 unsigned int g_iForcePollCycles = 10;
+//
+void PollAllControls()
+{
+    //dopplerLandSeaMode.pollInputCurrent(); Doesnt matter
+    radarGain.pollInputCurrent();
+    radarPulseNormalShort.pollInputCurrent();
+    radarRecceOnOff.pollInputCurrent();
+    masterModeSelect.pollInputCurrent();
+    antiJamMode.pollInputCurrent();
+ }
 
-void setup() {
-  DcsBios::setup();
-}
+//---------------- Panelbelysning ------------------------------------------------------
+bool PanelLed_Power;
+unsigned int PanelLed_Value;
 
+//----*Check Main Power Switch
 void onMainElectricPowerChange(unsigned int newValue) {
-    if (newValue==0){
-      PanelLed_Value = 0; }
+      PanelLed_Power = newValue; 
 }
 DcsBios::IntegerBuffer mainElectricPowerBuffer(0x4606, 0x0010, 4, onMainElectricPowerChange);
-  
+
+//----*Read Panel Lights Value
 void onPanelLightsChange(unsigned int newValue) {
-      PanelLed_Value = map(newValue, 0, 65535, 0, 200);
-     analogWrite(PanelLight_pin, PanelLed_Value);
+ PanelLed_Value = newValue;
 }
 DcsBios::IntegerBuffer panelLightsBuffer(0x4678, 0xffff, 0, onPanelLightsChange);
 
+//----*Set LED Panel Lights
+void setPanelLights(unsigned int PanelLed_Value){
+   if(!PanelLed_Power){
+    PanelLed_Value = 0;
+  }
+  else {
+      PanelLed_Value = map(PanelLed_Value, 0, 65535, 0, 200);
+  }
+     analogWrite(PanelLight_pin, PanelLed_Value); 
+}
+//---------------Panelbelysning Slut-------------------------------------------
 
+void setup() {
+  DcsBios::setup();
+  pinMode(PanelLight_pin, OUTPUT);
+}
 
 void loop() {
     DcsBios::loop();
-       if ( g_iForcePollCycles > 0 )
+    setPanelLights(PanelLed_Value);
+     if ( g_iForcePollCycles > 0 )
     { 
         //repeat poll for this many cycles
-        if ( ++g_iInitIntervalCounter == 0 ) 
+        if ( ++g_iInitIntervalCounter == 4 ) 
         {  PollAllControls();     //main loop 1->65535->0 then polls
            g_iForcePollCycles--;
         }
@@ -59,12 +85,3 @@ void loop() {
     }
 
 }
-
-void PollAllControls()
-{
-    radarGain.pollInputCurrent();
-    radarPulseNormalShort.pollInputCurrent();
-    radarRecceOnOff.pollInputCurrent();
-    masterModeSelect.pollInputCurrent();
-    antiJamMode.pollInputCurrent();
- }
