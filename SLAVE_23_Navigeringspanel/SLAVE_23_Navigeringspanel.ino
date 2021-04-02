235c717ca83bbebc1f54c2e7adb078da07c7e0dd
Gybb <fredrik_ljung@live.se>
2021-04-02 22:04:48 +0200

    Panel Lights work as intended

--- SLAVE_23_Navigeringspanel/SLAVE_23_Navigeringspanel.ino
+++ SLAVE_23_Navigeringspanel/SLAVE_23_Navigeringspanel.ino
@@ -1,9 +1,8 @@
-//#define DCSBIOS_IRQ_SERIAL
 #define DCSBIOS_RS485_SLAVE 23
 #define TXENABLE_PIN 2
 
 #include "DcsBios.h"
-
+//******************* NAVPANEL ******************************
 DcsBios::Switch2Pos dmeSelector("DME_SELECTOR", 13);
 
 DcsBios::Switch2Pos radarAltimeterPower("RADAR_ALTIMETER_POWER", 12);
@@ -37,37 +36,57 @@ DcsBios::Switch2Pos navSelectBtnLMal("NAV_SELECT_BTN_L_MAL", A2);
 DcsBios::Switch2Pos navSelectBtnLs("NAV_SELECT_BTN_LS", A3);
 
 DcsBios::AnalogMultiPos tilsChannelSelect("TILS_CHANNEL_SELECT", A7, 11, (1023/11));
-//*******************NAVPANEL SLUT******************************
+//******************* NAVPANEL SLUT ******************************
 //----------Check switch states polls-----------------
 unsigned int g_iInitIntervalCounter = 0;
 unsigned int g_iForcePollCycles = 10;
+void PollAllControls()
+{
+    dmeSelector.pollInputCurrent();
+    radarAltimeterPower.pollInputCurrent();
+    tilsChannelSelect.pollInputCurrent();
+  }
 
-void setup() {
-  
-  DcsBios::setup();
- //  pinMode(PanelLight_pin, OUTPUT);
-}
+//---------------- Panelbelysning ------------------------------------------------------
+bool PanelLed_Power;
+unsigned int PanelLed_Value;
 
+//----*Check Main Power Switch
 void onMainElectricPowerChange(unsigned int newValue) {
-    if (newValue==0){
-      PanelLed_Value = 0; }
+      PanelLed_Power = newValue; 
 }
 DcsBios::IntegerBuffer mainElectricPowerBuffer(0x4606, 0x0010, 4, onMainElectricPowerChange);
-  
+
+//----*Read Panel Lights Value
 void onPanelLightsChange(unsigned int newValue) {
-      PanelLed_Value = map(newValue, 0, 65535, 0, 200);
-     analogWrite(PanelLight_pin, PanelLed_Value);
+ PanelLed_Value = newValue;
 }
 DcsBios::IntegerBuffer panelLightsBuffer(0x4678, 0xffff, 0, onPanelLightsChange);
 
+//----*Set LED Panel Lights
+void setPanelLights(unsigned int PanelLed_Value){
+   if(!PanelLed_Power){
+    PanelLed_Value = 0;
+  }
+  else {
+      PanelLed_Value = map(PanelLed_Value, 0, 65535, 0, 200);
+  }
+     analogWrite(PanelLight_pin, PanelLed_Value); 
+}
+//---------------Panelbelysning Slut-------------------------------------------
 
+void setup() {
+  DcsBios::setup();
+  pinMode(PanelLight_pin, OUTPUT);
+}
 
 void loop() {
-  DcsBios::loop();
-         if ( g_iForcePollCycles > 0 )
+    DcsBios::loop();
+    setPanelLights(PanelLed_Value);
+     if ( g_iForcePollCycles > 0 )
     { 
         //repeat poll for this many cycles
-        if ( ++g_iInitIntervalCounter == 0 ) 
+        if ( ++g_iInitIntervalCounter == 23 ) 
         {  PollAllControls();     //main loop 1->65535->0 then polls
            g_iForcePollCycles--;
         }
@@ -75,10 +94,3 @@ void loop() {
     }
 
 }
-
-void PollAllControls()
-{
-    dmeSelector.pollInputCurrent();
-    radarAltimeterPower.pollInputCurrent();
-    tilsChannelSelect.pollInputCurrent();
-  }
